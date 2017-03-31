a5980ac62ecca763780cfa1d27a89598a387c276
yadi mulyadi <yadi.mulyadi@mainspring.technology>
2017-03-31 14:41:05 +0700

    add grapihite to servive

--- src/main/scala/id/co/babe/entityextractor/module/GraphiteMetricsModule.scala
+++ src/main/scala/id/co/babe/entityextractor/module/GraphiteMetricsModule.scala
@@ -4,10 +4,11 @@ package id.co.babe.entityextractor.module
   * Created by MG-5210 on 3/31/2017.
   */
 import java.net.UnknownHostException
+
 import scala.sys.process._
 import java.util.concurrent.TimeUnit
 
-import com.codahale.metrics.graphite.{GraphiteReporter, PickledGraphite}
+import com.codahale.metrics.graphite.{Graphite, GraphiteReporter, PickledGraphite}
 import com.google.inject.Provides
 import com.twitter.finagle.metrics.MetricsStatsReceiver
 import com.twitter.inject.{Injector, Logging, TwitterModule}
@@ -18,25 +19,25 @@ import com.typesafe.config.Config
   */
 object GraphiteMetricsModule extends TwitterModule with Logging{
 
-//  @Provides
-//  def graphite(config: Config) = {
-//    val host = config.getString("metrics.host")
-//    val port = config.getInt("metrics.port")
-//
-//    new Graphite(host, port)
-//  }
+  //  @Provides
+  //  def graphite(config: Config) = {
+  //    val host = config.getString("metrics.host")
+  //    val port = config.getInt("metrics.port")
+  //
+  //    new Graphite(host, port)
+  //  }
 
   /**
     * Use PickledGraphite to send metrics in batches
     */
-    @Provides
-    def pickledGraphite(config: Config) = {
-      val host = config.getString("metrics.host")
-      val port = config.getInt("metrics.port")
-      val batch = config.getInt("metrics.batch_size")
+  @Provides
+  def pickledGraphite(config: Config) = {
+    val host = config.getString("metrics.host")
+    val port = config.getInt("metrics.port")
+    val batch = config.getInt("metrics.batch_size")
 
-      new PickledGraphite(host, port,batch)
-    }
+    new PickledGraphite(host, port, batch)
+  }
 
   @Provides
   def reporter(graphite: PickledGraphite, config: Config) = {
@@ -47,10 +48,10 @@ object GraphiteMetricsModule extends TwitterModule with Logging{
       throw new UnknownHostException("exec command \"hostname\" doesn't yield anything")
 
     GraphiteReporter.forRegistry(MetricsStatsReceiver.metrics)
-      .prefixedWith(s"$prefix.$hostname")
-      .convertRatesTo(TimeUnit.SECONDS)
-      .convertDurationsTo(TimeUnit.MILLISECONDS)
-      .build(graphite)
+        .prefixedWith(s"$prefix.$hostname")
+        .convertRatesTo(TimeUnit.SECONDS)
+        .convertDurationsTo(TimeUnit.MILLISECONDS)
+        .build(graphite)
   }
 
   override def singletonPostWarmupComplete(injector: Injector): Unit = {
